67ba1e7fe1bf94f4529219747f7c84345ba5f13e
Ben Smith <benjsmi@us.ibm.com>
2015-12-04 13:00:27 -0500

    Make it so you can build without having to have DOCKER_HOST set. Also fix env vars for logstash s.t. startup creates the key and cert.

--- player-wlpcfg/startup.sh
+++ player-wlpcfg/startup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ "$LOGGING_DOCKER_HOST" != "" ]; then
+if [ "$LOGSTASH_ENDPOINT" != "" ]; then
   /opt/ibm/wlp/bin/server start defaultServer
   echo Starting the logstash forwarder...
-  wget https://admin:$ADMIN_PASSWORD@game-on.org:8443/logstashneeds.tar -O /opt/logstashneeds.tar
-  sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
-  tar xvzf logstashneeds.tar ; rm logstashneeds.tar
-  cd /opt ; chmod +x ./forwarder ; tar xvzf logstashneeds.tar ; rm logstashneeds.tar
+  sed -i s/PLACEHOLDER_LOGHOST/$LOGSTASH_ENDPOINT/g /opt/forwarder.conf
+  cd /opt
+  chmod +x ./forwarder
+  echo -e $LOGSTASH_CERT > logstash-forwarder.crt
+  echo -e $LOGSTASH_KEY > logstash-forwarder.key
+  sleep 0.5
   ./forwarder --config ./forwarder.conf
 else
   /opt/ibm/wlp/bin/server run defaultServer
