4209a6b57af3644e3745a19ff422c2d75c2ed7d4
Adam Bien <abien@adam-bien.com>
2016-02-03 14:52:27 +0100

    superfluous web.xml removed, annotation added

--- player-app/src/main/java/net/wasdev/gameon/cors/CORSFilter.java
+++ player-app/src/main/java/net/wasdev/gameon/cors/CORSFilter.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/** *****************************************************************************
  * Copyright (c) 2015 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,19 +12,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
-package net.wasdev.gameon;
+ ****************************************************************************** */
+package net.wasdev.gameon.cors;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
+@WebFilter(urlPatterns = "/*")
 public class CORSFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
