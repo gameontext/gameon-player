3cb0944929ce78ecea1eff4886f631a03fbaf92f
Erin Schnabel <schnabel@us.ibm.com>
2016-06-21 14:40:32 -0400

    report docker build failures
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- idsBuild.sh
+++ idsBuild.sh
@@ -6,14 +6,7 @@
 
 #!/bin/bash
 echo Informing Slack...
-curl -X 'POST' --silent --data-binary '{"text":"A new build for the player service has started."}' $WEBHOOK > /dev/null
-
-echo Setting up Docker...
-mkdir dockercfg ; cd dockercfg
-echo -e $KEY > key.pem
-echo -e $CA_CERT > ca.pem
-echo -e $CERT > cert.pem
-cd ..
+curl -X 'POST' --silent --data-binary '{"text":"A new build for the player service has started."}' $SLACK_WEBHOOK_PATH > /dev/null
 
 echo Building projects using gradle...
 ./gradlew build 
@@ -21,15 +14,36 @@ rc=$?
 if [ $rc != 0 ]
 then
   echo "Gradle build failed, will NOT perform Docker steps."
+  curl -X 'POST' --silent --data-binary '{"text":"Build for the player service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
   exit 1
 fi
 
-echo Building and Starting Concierge Docker Image...
+echo Setting up Docker...
+mkdir dockercfg ; cd dockercfg
+echo -e $KEY > key.pem
+echo -e $CA_CERT > ca.pem
+echo -e $CERT > cert.pem
+
+echo $PWD
+echo $WORKSPACE/dockercfg
+cd ..
+
+export DOCKER_CERT_PATH=$WORKSPACE/dockercfg
+echo Building and Starting Docker Image using $DOCKER_CERT_PATH
+
 cd player-wlpcfg
 
-../gradlew buildDockerImage 
-../gradlew stopCurrentContainer 
-../gradlew removeCurrentContainer
-../gradlew startNewEtcdContainer
+../gradlew --no-daemon buildDockerImage 
+rc=$?
+if [ $rc != 0 ]
+then
+  echo "Gradle build failed, will NOT perform Docker steps."
+  curl -X 'POST' --silent --data-binary '{"text":"Build for the player service has failed."}' $SLACK_WEBHOOK_PATH > /dev/null
+  exit 1
+fi
+
+../gradlew --no-daemon stopCurrentContainer 
+../gradlew --no-daemon removeCurrentContainer
+../gradlew --no-daemon startNewEtcdContainer
 
 rm -rf dockercfg
