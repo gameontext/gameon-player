addbaaae9361bbd39617ab47bf78391214d7a5b9
Erin Schnabel <schnabel@us.ibm.com>
2016-08-29 15:29:50 -0400

    local dev dropin

--- player-app/src/main/java/org/gameontext/player/PlayerLocationsResource.java
+++ player-app/src/main/java/org/gameontext/player/PlayerLocationsResource.java
@@ -25,7 +25,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
